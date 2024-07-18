84cdbfadba3dea6f423f6277a3d9ec10a116c760
haidarRamadhan <haidar_ramadhan_@teknokrat.ac.id>
2024-07-18 22:17:02 +0700

    second change

--- adminpanel/login.php
+++ adminpanel/login.php
@@ -58,7 +58,7 @@ require "../js/koneksi.php";
                     else {
                         ?>
                         <div class="alert alert-warning" role="alert">
-                            password salah !
+                            password salah KUCOBA !
                     </div>
                         <?php
                     }
